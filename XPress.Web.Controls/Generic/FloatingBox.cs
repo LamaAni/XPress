14bdcb94736412e6878f7ac272b18b324c0f8860
LamaAni <Zav.Shotan@GMail.com>
2014-02-09 18:44:07 +0200

    Added base for floating controls.

--- XPress.Web.Controls/Generic/FloatingBox.cs
+++ XPress.Web.Controls/Generic/FloatingBox.cs
@@ -9,8 +9,8 @@ namespace XPress.Web.Controls.Generic
     /// <summary>
     /// Implements a floating box, used to create a floating windows and other floating controls.
     /// </summary>
+    [XPress.Web.Links.Attributes.LinkConstructor("XPress.Web.Controls.Generic.FloatingBox.js", Links.LinkOrigin.Embedded)]
     public class FloatingBox : XPress.Web.Controls.XPressControl
     {
-
     }
 }
